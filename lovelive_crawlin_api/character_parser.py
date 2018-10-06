7c5e8d39bcdce0870ed4a63a67916e19dee6093c
tails5555 <tails5555@naver.com>
2018-10-06 19:16:00 +0900

    Create CharacterMainInfoModel at character_parser 캐릭터 간략 정보에 대해 얻어오는 크롤링 함수 중 이에 대한 DTO 클래스 생성을 하였음. 작동 여부는 인벤에 있는 사이트를 통해 테스트 진행함.

--- lovelive_crawlin_api/character_parser.py
+++ lovelive_crawlin_api/character_parser.py
@@ -9,6 +9,57 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lovelive_crawlin_api.settings")
 import django
 django.setup()
 
+NAME_IDX = 0
+VOICE_ACTOR_IDX = 1
+GRADE_IDX = 2
+BIRTHDAY_IDX = 3
+HEIGHT_IDX = 4
+THREE_SIZE_IDX = 5
+BLOOD_TYPE_IDX = 6
+HOBBIES_IDX = 7
+
+class CharacterMainInfoModel :
+    def __init__(self, kor_name='', jap_name='', voice_actor='', grade='', birthday='', height='', three_size='', blood_type='', hobbies='') :
+        self.kor_name = kor_name
+        self.jap_name = jap_name
+        self.voice_actor = voice_actor
+        self.grade = grade
+        self.birthday = birthday
+        self.height = height
+        self.three_size = three_size
+        self.blood_type = blood_type
+        self.hobbies = hobbies
+    
+    def set_kor_name(self, kor_name) :
+        self.kor_name = kor_name
+    
+    def set_jap_name(self, jap_name) :
+        self.jap_name = jap_name
+
+    def set_voice_actor(self, voice_actor) :
+        self.voice_actor = voice_actor
+    
+    def set_grade(self, grade) :
+        self.grade = grade
+
+    def set_birthday(self, birthday) :
+        self.birthday = birthday
+
+    def set_height(self, height) :
+        self.height = height
+
+    def set_three_size(self, three_size) :
+        self.three_size = three_size
+
+    def set_blood_type(self, blood_type) :
+        self.blood_type = blood_type
+
+    def set_hobbies(self, hobbies) :
+        self.hobbies = hobbies
+
+    def __str__(self) :
+        return 'korName/{} japName/{} voiceActor/{}\ngrade/{} birthday/{} height/{}\nthreeSize/{} bloodType/{} hobbies/{}\n'.format(self.kor_name, self.jap_name, self.voice_actor, self.grade, self.birthday, self.height, self.three_size, self.blood_type, self.hobbies)
+
 def parse_card_id_list() :
     req = requests.get('http://lovelive.inven.co.kr/dataninfo/card/')
     html = req.text
@@ -25,7 +76,7 @@ def parse_card_id_list() :
                 idx_td = card_tr.find('td', {'class': 'ucode'})
                 idx = idx_td.find(text=True) if idx_td != None else ''
                 if idx.strip() != '' :
-                    search_card_nos.append(idx)
+                    search_card_nos.append(int(idx))
 
             return search_card_nos
 
@@ -37,8 +88,36 @@ def parse_card_id_list() :
 
 def parse_character_info(card_no) :
     url = 'http://lovelive.inven.co.kr/dataninfo/card/detail.php?d=2&c={0}'.format(card_no)
-    print(url)
+    req = requests.get(url)
+    html = req.text
+    if req.status_code == 200 : 
+        
+        soup = BeautifulSoup(html, 'html.parser')
+        character_intro_div = soup.find('div', {'class': 'characterData'})
+        character_table = character_intro_div.find('table')
+        characterInfo = CharacterMainInfoModel()
+
+        for idx, card_td in enumerate(character_table.find_all('td')) :
+            if idx == NAME_IDX :
+                print(card_td.find(text=True))
+            elif idx == VOICE_ACTOR_IDX :
+                print(card_td.find(text=True))
+            elif idx == GRADE_IDX :
+                print(card_td.find(text=True))
+            elif idx == BIRTHDAY_IDX :
+                print(card_td.find(text=True))
+            elif idx == HEIGHT_IDX :
+                print(card_td.find(text=True))
+            elif idx == THREE_SIZE_IDX :
+                print(card_td.find(text=True))
+            elif idx == BLOOD_TYPE_IDX :
+                print(card_td.find(text=True))
+            elif idx == HOBBIES_IDX :
+                print(card_td.find(text=True))
 
 if __name__ == '__main__' :
-    pool = Pool(processes=4)
-    pool.map(parse_character_info, parse_card_id_list())
+    
+    ## pool = Pool(processes=4)
+    ## print(pool.map(parse_character_info, parse_card_id_list()))
+
+    parse_character_info(1535)
