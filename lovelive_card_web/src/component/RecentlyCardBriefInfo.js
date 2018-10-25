da71e23bd0a49123d3e00fd9f67906f7886c7415
tails5555 <tails5555@naver.com>
2018-10-25 15:50:02 +0900

    Update RecentlyCardBriefInfo Component 최근에 올라온 카드 정보에 마우스를 올리면 마우스 팝업 정보가 뜨도록 구현하였음. 모바일에서는 적용 안 시킴.

--- lovelive_card_web/src/component/RecentlyCardBriefInfo.js
+++ lovelive_card_web/src/component/RecentlyCardBriefInfo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { ListGroupItem, Media } from 'reactstrap';
 import { withRouter } from 'react-router-dom';
+import PopupCardInfo from './PopupCardInfo';
 
 const IMAGE_ICON_URL = `http://localhost:8000/card_icons/`
 
@@ -9,7 +10,7 @@ class RecentlyCardBriefInfo extends React.Component {
     constructor(props){
         super(props);
         this._isMounted = false;
-        this.state = { info : props.info, icons : [] };
+        this.state = { info : props.info, icons : [], mouseOn : false, mouseX : 0, mouseY : 0 };
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
@@ -52,30 +53,60 @@ class RecentlyCardBriefInfo extends React.Component {
         this.props.history.push(`/card/info?pg=1&id=${infoNo}`)
     }
 
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    handleMouseEnter = () => { 
+        this.setState({
+            mouseOn : true
+        });
+    }
+    
+    handleMouseLeave = () => {
+        this.setState({
+            mouseOn : false
+        });
+    }
+
+    handleMouseMove = (event) => {
+        this.setState({ 
+            mouseX : event.clientX, 
+            mouseY : event.clientY 
+        });
+    }
+
     render(){
-        const {info, icons} = this.state;
+        const { info, icons, mouseOn, mouseX, mouseY } = this.state;
+        let smallCardInfo = null;
+        if(mouseOn) {
+            smallCardInfo = <PopupCardInfo property={info && info.property} character={`${(info && info.character_name)} ${(info && info.japanese_name)}`} no={info && info.no} mouseX={mouseX} mouseY={mouseY} />;    
+        }
         return(
             <ListGroupItem>
-                <Media>
+                <Media style={{ cursor : 'pointer' }} onClick={() => this.handleClickPushCardInfo(info.no)} onTouchStart={() => this.handleClickPushCardInfo(info.no)} onMouseEnter={() => this.handleMouseEnter(info && (info.no || 0))} onMouseLeave={() => this.handleMouseLeave()} onMouseMove={this.handleMouseMove.bind(this)}>
                     {
                         window.innerWidth > 1024 ?
-                            <Media left onClick={() => this.handleClickPushCardInfo(info.no)}>
+                            <Media left>
                                 {icons.map((icon, idx) => <img src={icon} className="img-fluid" key={`recently_card_icon_${info && info.no}_${idx}`} alt={`recently_card_icon_${info && info.no}_${idx}`} /> )}
                             </Media> :
-                            <Media body onClick={() => this.handleClickPushCardInfo(info.no)}>
+                            <Media body>
                                 {icons.map((icon, idx) => <img src={icon} className="img-fluid" key={`recently_card_icon_${info && info.no}_${idx}`} alt={`recently_card_icon_${info && info.no}_${idx}`} /> )}
                             </Media>
                     }
                     
                     {
-                        window.innerWidth < 1024 ? null : 
-                            <Media body>
+                        window.innerWidth < 1024 ? 
+                            null : 
+                            <Media body style={mouseOn ? { textDecoration : 'underline' } : null}>
                                 <Media heading>
                                 { info && info.character_name }
                                 </Media>
                                 { info && info.card_title !== '' ? info.card_title : info && info.japanese_name }
                             </Media>
                     }
+
+                    { smallCardInfo }
                 </Media>       
             </ListGroupItem>
         )
